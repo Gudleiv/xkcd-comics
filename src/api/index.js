7fd7e6ddb1df7e3584a1de9573d4dece2066321e
Gleb Gudleiv <glebgudleiv@gmail.com>
2021-03-01 15:49:53 +0300

    add 404 comics

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,16 @@
+import comics404 from './../assets/img/404.jpg'
+
 const getComics = async (num) => {
   const proxy = 'http://127.0.0.1:8081/' // proxy cors
   let url = `https://xkcd.com/${num}/info.0.json`
   if (typeof(num) !== 'number' || num === 0) url = 'https://xkcd.com/info.0.json'
 
   const response = await fetch(proxy + url)
-  if (response.status === 404) return { img: '/img/404.jpg', num: 404 }
+  if (response.status === 404) return {
+    num: 404,
+    img: comics404,
+    titile: '404 - Comics not found'
+  }
   const data = await response.json()
   return data
 }
